21240b3ac187f5b1104eb49f277196e0ca438cbd
AlanGlezz <105512732+20213tn113@users.noreply.github.com>
2024-04-14 20:30:06 -0600

    Update config.py

--- config.py
+++ config.py
@@ -5,9 +5,9 @@ class Config:
 
 class DevelopmentConfig(Config):
     DEBUG = True
-    MYSQL_HOST='localhost'
-    MYSQL_USER='root'
-    MYSQL_PASSWORD=''
+    MYSQL_HOST='bd-sicovo.cqzjefqs7sil.us-east-1.rds.amazonaws.com'
+    MYSQL_USER='sicovo'
+    MYSQL_PASSWORD='sicovo12'
     MYSQL_DB='tienda'
     MAIL_SERVER = 'smtp.googlemail.com'
     MAIL_PORT = 587
